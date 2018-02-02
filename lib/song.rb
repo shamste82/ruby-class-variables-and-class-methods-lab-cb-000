26aa1e97ce9502d1a06708e49e9f45b7db2f2d2b
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-02-02 03:35:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
 
     @@artists.each do |x|
       if x == @artist
-        artist_count["Metallica"] += 1
+        artist_count[@artist] += 1
       else
         artist_count[x] = 1
       end
